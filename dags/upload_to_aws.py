8c6d6a9a66c388ca11fcfc63e94e59b16becd165
Elvin <efeng92@gmail.com>
2022-04-05 21:43:25 -0400

    React update

--- dags/upload_to_aws.py
+++ dags/upload_to_aws.py
@@ -9,6 +9,7 @@ from airflow.exceptions import AirflowException
 
 
 AWS_DDB_URL = 'https://p5v5a0pnfi.execute-api.us-east-1.amazonaws.com/v1/data'
+AWS_SQS_URL = 'https://p5v5a0pnfi.execute-api.us-east-1.amazonaws.com/v1/sqs'
 
 
 @task
@@ -24,9 +25,13 @@ def upload_league_data_to_dynamo(data: dict):
   headers = {'content-type': 'application/json'}
   payload = json.dumps(data)
 
+  # Write to SQS first prior to dynamodb to prevent throttling, with some delay
+  sleep(randint(0, 60))
+  r = requests.put(AWS_SQS_URL, data=payload, headers=headers)
+
   # Random sleep (seconds) to prevent dynamodb write throttling
-  sleep(randint(0, 900))
-  r = requests.put(AWS_DDB_URL, data=payload, headers=headers)
+  #sleep(randint(0, 900))
+  #r = requests.put(AWS_DDB_URL, data=payload, headers=headers)
 
   if r.status_code == 500:
     raise AirflowException("Error uploading to dynamodb")
