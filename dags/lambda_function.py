b1fe7e768953c30071f555c7d498d57aa5ddcd50
Elvin <efeng92@gmail.com>
2022-05-06 23:19:37 -0400

    lambda updates

--- dags/lambda_function.py
+++ dags/lambda_function.py
@@ -4,5 +4,7 @@ from extract_espn import (
 
 def lambda_handler(event, context):
   league_id = event["queryStringParameters"]['leagueId']
+  league_year = event["queryStringParameters"]['leagueYear']
 
-  print(league_id)
+  print(event)
+  print(context)
