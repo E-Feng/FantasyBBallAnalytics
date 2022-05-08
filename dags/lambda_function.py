fc6f48d157b20fb269699630f004ee020c252b7f
Elvin <efeng92@gmail.com>
2022-05-08 16:38:39 -0400

    merging df for draft

--- dags/lambda_function.py
+++ dags/lambda_function.py
@@ -73,8 +73,8 @@ def lambda_handler(event, context):
       data_endpoint = extract_from_espn_api(league_info, view, header)
       league_data[endpoint] = transform_raw_to_df(endpoint, data_endpoint)
 
-  has_ejections_cat = int(consts.EJS) in league_data['settings']['categoryIds']
-  print("has ejections ", has_ejections_cat)
+    has_ejections_cat = int(consts.EJS) in league_data['settings']['categoryIds']
+    print("has ejections ", has_ejections_cat)
 
-  full_draft_recap = pd.merge(league_data['draftRecap'], league_data['ratings'], how='left', on='playerId')
+    full_draft_recap = pd.merge(league_data['draftRecap'], league_data['ratings'], how='left', on='playerId')
 
