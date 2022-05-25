260f7c0cffdb860d84a79550c6031b927cbd22bd
Elvin <efeng92@gmail.com>
2022-05-25 19:38:34 -0400

    lambda clean up

--- dags/lambda_function.py
+++ dags/lambda_function.py
@@ -101,15 +101,3 @@ def lambda_handler(event, context):
     'statusCode': 200,
     'body': "Test response"
   }
-
-""" 
-event = {
-  "queryStringParameters": {
-    "leagueId": '48375511',
-    "cookieEspnS2": "AEANBh%2BD2CyE%2BH%2FBEYL2sJ%2B4nV9%2FOklCUoyYPiegbqwlFqzfE%2BnViiqW87jner2OdiFYVXKnHjjaSSx%2FJDbZWgyrFSCnaU8AxPJtsGXuMpDzFZw7B8YgcpTmCkSasag97Sd%2Fl1r6igCZh%2F1YyquO0H%2FyMVIXq8%2FUAarrXIFzeSx%2BBiB0ywQn6Iz6Smkiv63RWoJeNrzojIXfuoTbFw%2BVzXSnF6TH5MF4X7ooRKw%2FImPagScBbqIMjrq0EfPf6%2Bcm9XE%3D",
-    "cookieSwid": "A746C402-08B1-42F4-86C4-0208B142F42A"
-  }
-}
-context = None
-lambda_handler(event, context) 
-"""
