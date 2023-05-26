53754fa373dacdf259058291710bcd9b88072f2c
Elvin <efeng92@gmail.com>
2023-05-25 20:27:08 -0400

    Fixing stopiteration exception

--- dags/transform_raw_data.py
+++ dags/transform_raw_data.py
@@ -213,7 +213,7 @@ def transform_players_to_df(ratings: dict):
         row['statRatings' + period] = format_stat_ratings(player['ratings']['0']['statRankings'])
 
       # Stats, dynamic filtering out right dict that matches id field
-      stats_period = next((d for d in player['player']['stats'] if d['id'] == f'0{key}2023'), None)
+      stats_period = next((d for d in player['player']['stats'] if d['id'] == f'0{key}2023'), {})
       if stats_period.get('averageStats'):
         row['stats' + period] = stats_period['averageStats']
 
