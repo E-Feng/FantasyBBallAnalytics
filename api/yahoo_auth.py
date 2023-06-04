c69868bc51e73f7993c3b23304a48fbfb0ecbafe
Elvin <efeng92@gmail.com>
2023-06-03 23:46:16 -0400

    auth methods

--- api/yahoo_auth.py
+++ api/yahoo_auth.py
@@ -1,15 +1,36 @@
+import boto3
 import requests
 
+from util import invoke_lambda
+
+
+lambda_client = boto3.client("lambda", region_name="us-east-1")
+
+yahoo_key = invoke_lambda(lambda_client, "get_secret", {"key": "yahoo_key"})
+yahoo_secret = invoke_lambda(lambda_client, "get_secret", {"key": "yahoo_secret"})
+
 url = "https://api.login.yahoo.com/oauth2/get_token"
-
-YAHOO_KEY="dj0yJmk9Y0NLbVduVG0wTFdBJmQ9WVdrOWJYVkJSbG8wYTNjbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTYz"
-YAHOO_SECRET="8cc969af8890f8775d55367728267d6907a0b4c7"
-
-YAHOO_AUTH_CODE="k4yae4m"
-
-payload = f"client_id={YAHOO_KEY}&grant_type=authorization_code&code={YAHOO_AUTH_CODE}&redirect_uri=oob&client_secret={YAHOO_SECRET}"
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+def get_yahoo_access_token(event, context):
+    yahoo_auth_code = event["queryStringParameters"].get("yahooAuthCode")
+    yahoo_refresh_token = event["queryStringParameters"].get("yahooRefreshToken")
+
+    if not yahoo_refresh_token:
+        get_payload = f"client_id={yahoo_key}&grant_type=authorization_code&code={yahoo_auth_code}&redirect_uri=oob&client_secret={yahoo_secret}"
+
+        res = requests.request("POST", url, headers=headers, data=get_payload)
+
+    else:
+        refresh_payload = f"client_id={yahoo_key}&grant_type=refresh_token&redirect_uri=oob&refresh_token={yahoo_refresh_token}&client_secret={yahoo_secret}"
+
+        res = requests.request("POST", url, headers=headers, data=refresh_payload)
+
+    if res.status_code == 200:
+        yahoo_access_token = res.json()["access_token"]
+        yahoo_refresh_token = res.json()["refresh_token"]
+
+        return [yahoo_access_token, yahoo_refresh_token]
+    
+    return res.json()
