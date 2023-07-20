d1101ccd3448cc9e3ac9547531984f9915cea50f
Elvin <efeng92@gmail.com>
2023-07-20 00:19:50 -0400

    fixing sql file name

--- api/sql/update_espn_league_after_process copy.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO leagueids(
-    leagueid, created, lastupdated, lastviewed, platform, viewcount, active, cookieespns2)
-VALUES (%(league_id)s, CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, %(platform)s, 0, TRUE, %(cookie_espn)s)
-ON CONFLICT (leagueid, platform) DO UPDATE SET
-    lastupdated = NOW(), 
-    viewCount = leagueids.viewCount + 1,
-    cookieespns2 = EXCLUDED.cookieespns2
