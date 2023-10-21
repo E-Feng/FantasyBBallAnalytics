8ef6f2b59958a283f51ff945c0da673d16548d82
Elvin <efeng92@gmail.com>
2023-10-21 17:45:36 -0400

    using timestamps

--- api/sql/update_espn_league_after_process.sql
+++ api/sql/update_espn_league_after_process.sql
@@ -1,6 +1,6 @@
 INSERT INTO leagueids(
     leagueid, created, lastupdated, lastviewed, platform, viewcount, active, cookieespns2)
-VALUES (%(league_id)s, CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, 'espn', 0, TRUE, %(cookie_espn)s)
+VALUES (%(league_id)s, CURRENT_DATE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'espn', 0, TRUE, %(cookie_espn)s)
 ON CONFLICT (leagueid, platform) DO UPDATE SET
     lastupdated = NOW(), 
     viewCount = leagueids.viewCount + 1,
