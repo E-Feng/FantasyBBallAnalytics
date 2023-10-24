efd123e14796ae696cffd0cc9a2b4a4f870e9fa8
Elvin <efeng92@gmail.com>
2023-10-24 19:54:52 -0400

    work

--- client/src/containers/RosterContainer.js
+++ client/src/containers/RosterContainer.js
@@ -3,6 +3,16 @@ import styled from 'styled-components';
 
 
 function RosterContainer(props) {
+  const [period, setPeriod] = useState('Last15');
+  const [displayList, setDisplayList] = useState([])
+
+  const players = props.players;
+  const teams = props.teams;
+  const catIds = props.settings[0].categoryIds;
+
+  const periodArray = ['Last7', 'Last15', 'Last30', 'Season'];
+  const ratingsKey = `statRatings${period}`;
+
 
   return (
     <p></p>
