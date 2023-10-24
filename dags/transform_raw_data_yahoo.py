86dddcd0168a1311e8ea39c791914c228aa10b8b
Elvin <efeng92@gmail.com>
2023-10-23 21:12:49 -0400

    fix yahoo cat ids

--- dags/transform_raw_data_yahoo.py
+++ dags/transform_raw_data_yahoo.py
@@ -104,8 +104,10 @@ def transform_settings_to_df(data: dict):
         stat = stat["stat"]
 
         stat_id = str(stat["stat_id"])
-        stat_id_espn = int(consts.STAT_IDS_MAP_TO_ESPN.get(stat_id, -100))
-        row["categoryIds"].append(stat_id_espn)
+        stat_id_espn = int(consts.STAT_IDS_MAP_TO_ESPN.get(stat_id, -1))
+
+        if stat_id_espn >= 0:
+            row["categoryIds"].append(stat_id_espn)
 
     data_array.append(row)
 
