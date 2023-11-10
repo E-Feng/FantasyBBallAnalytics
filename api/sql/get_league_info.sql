5dbbf723cd23779c045a647fbe85c9587fcd5d9f
Elvin <efeng92@gmail.com>
2023-11-09 22:15:53 -0500

    league retrieval fix

--- api/sql/get_league_info.sql
+++ api/sql/get_league_info.sql
@@ -1,12 +1,7 @@
-WITH joinedleagueids AS (
-  SELECT
-    (NOW() - lastupdated) < INTERVAL '1 day',
-    CASE WHEN platform = 'espn' THEN cookieespns2 ELSE yahoorefreshtoken END,
-    platform,
-    COALESCE(l2.linkedid, l1.leagueid) as leagueid
-  FROM leagueids l1
-  LEFT JOIN linkedids l2 ON l1.leagueid=l2.mainid
-)
-SELECT *
-FROM joinedleagueids
+SELECT
+  (NOW() - lastupdated) < INTERVAL '1 day',
+  CASE WHEN platform = 'espn' THEN cookieespns2 ELSE yahoorefreshtoken END,
+  platform,
+  leagueid
+FROM leagueids
 WHERE leagueid ~ %(league_id_re)s
